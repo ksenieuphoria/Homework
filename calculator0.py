2b75035ef781570a4d68cf4f69bb28c370477f62
Ksusha <ksushaspring@gmail.com>
2022-05-31 15:53:09 +0300

    finish

---
+++ calculator0.py
@@ -0,0 +1,24 @@
+while True:
+    n1 = int(input('Введите число: '))
+    n2 = int(input('Введите второе число: '))
+    s = input('Что сделать с числом? (+,-,*,/): ')
+    while True:
+        if s == '+':
+            print('Сумма чисел равна: ' + str(n1 + n2))
+            break
+        elif s == '-':
+            print('Разность чисел равна: ' + str(n1 - n2))
+            break
+        elif s == '*':
+            print('Произведение чисел равно: ' + str(n1 * n2))
+            break
+        elif s == '/':
+            if n2 != 0:
+                print('Разность чисел равна: ' + str(n1 / n2))
+                break
+            else:
+                print('Деление на ноль!')
+                break
+        else:
+            print('Неверный знак операции!')
+            s = input('Что сделать с числом? (+,-,*,/): ')
