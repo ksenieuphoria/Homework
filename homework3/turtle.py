0937706daac38f97a045d8abdc7554537b93a29e
ksenieuphoria <105662732+ksenieuphoria@users.noreply.github.com>
2022-05-31 15:35:14 +0300

    Add files via upload

---
+++ homework3/turtle.py
@@ -0,0 +1,33 @@
+import turtle
+
+window = turtle.Screen()
+window.bgcolor('silver')
+window.setup(700, 700)
+window.title("Черепашка")
+
+turtle.shape("turtle")
+
+turtle.color('green')
+turtle.down()
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(150)
+turtle.up()
+turtle.forward(150)
+
+turtle.color('blue')
+turtle.down()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(270)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(270)
+turtle.up()
+turtle.forward(90)
+turtle.exitonclick()
